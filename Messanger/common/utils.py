58ef7f4f2992cca26655ec594fe2f227bbf225f7
Viktor <seal_skif@mail.ru>
2020-07-16 20:00:12 +0300

    Done with socket

--- Messanger/common/utils.py
+++ Messanger/common/utils.py
@@ -1,8 +1,12 @@
-import json
+"""Утилиты"""
 
+import json
+import sys
 from common.variables import ENCODING, MAX_PACKAGE_LENGTH
 from common.decorators import log
+from common.errors import *
 
+sys.path.append('../')
 
 @log
 def get_message(client):
@@ -15,8 +19,8 @@ def get_message(client):
         response = json.loads(json_response)
         if isinstance(response, dict):
             return response
-        raise ValueError
-    raise ValueError
+        raise IncorrectDataRecivedError
+    raise IncorrectDataRecivedError
 
 
 @log
@@ -24,6 +28,8 @@ def send_message(sock, message):
     """
     Функция кодирования и отправки сообщения.
     """
+    if not isinstance(message, dict):
+        raise NonDictInputError
     js_message = json.dumps(message)
     encoded_message = js_message.encode(ENCODING)
     sock.send(encoded_message)
